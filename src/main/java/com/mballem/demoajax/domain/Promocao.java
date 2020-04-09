e3c2469176304a4d1ec30ea168be8fa5e36cf8be
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-09 18:29:36 -0300

    A Entidade Promoção

---
+++ src/main/java/com/mballem/demoajax/domain/Promocao.java
@@ -0,0 +1,117 @@
+package com.mballem.demoajax.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+@Entity
+@Table(name = "promocoes")
+public class Promocao implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "titulo", nullable = false)
+	private String titulo;
+	
+	@Column(name = "link_promocao", nullable = false)
+	private String linkPromocao;
+	
+	@Column(name = "site_promocao", nullable = false)
+	private String site;
+	
+	@Column(name = "descricao")
+	private String descricao;
+	
+	@Column(name = "link_imagem", nullable = false)
+	private String linkImagem;
+	
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
+	@Column(name = "preco_promocao", nullable = false)
+	private BigDecimal preco;
+	
+	@Column(name = "total_likes")
+	private int likes;
+	
+	@Column(name = "data_cadastro", nullable = false)
+	private LocalDate dtCadastro;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getLinkPromocao() {
+		return linkPromocao;
+	}
+
+	public void setLinkPromocao(String linkPromocao) {
+		this.linkPromocao = linkPromocao;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getLinkImagem() {
+		return linkImagem;
+	}
+
+	public void setLinkImagem(String linkImagem) {
+		this.linkImagem = linkImagem;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+
+	public LocalDate getDtCadastro() {
+		return dtCadastro;
+	}
+
+	public void setDtCadastro(LocalDate dtCadastro) {
+		this.dtCadastro = dtCadastro;
+	}
+}
