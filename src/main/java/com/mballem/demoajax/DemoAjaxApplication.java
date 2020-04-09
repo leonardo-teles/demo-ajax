c1af5ba03d5ac88af83c4ac69ca09286fe7a727d
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-09 15:56:14 -0300

    Controller para as Meta Tags

--- src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
@@ -1,26 +1,12 @@
 package com.mballem.demoajax;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mballem.demoajax.domain.SocialMetaTag;
-import com.mballem.demoajax.service.SocialMetaTagService;
-
 @SpringBootApplication
-public class DemoAjaxApplication implements CommandLineRunner {
+public class DemoAjaxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAjaxApplication.class, args);
-	}
-
-	@Autowired
-	SocialMetaTagService service;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		SocialMetaTag tag = service.getSocialMetaTagByUrl("https://www.pichau.com.br/headset-logitech-g933-artemis-spectrum-7-1-surround-usb-wireless-981-000598");
-		System.out.println(tag.toString());
 	}
 }
