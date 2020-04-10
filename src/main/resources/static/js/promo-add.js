b6db5822e94a278aebf5486c908b79706ac4860c
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-10 02:14:52 -0300

    Trabalhando com o Loading

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -19,6 +19,10 @@ $("#form-add-promo").submit(function(evt) {
 		method: "POST",
 		url: "/promocao/save",
 		data: promo,
+		beforeSend: function() {
+			$("#form-add-promo").hide();
+			$("#loader-form").addClass("loader").show();
+		},
 		success: function() {
 			$("#form-add-promo").each(function() {
 				this.reset();
@@ -31,6 +35,12 @@ $("#form-add-promo").submit(function(evt) {
 		error: function(xhr) {
 			console.log('error > ', xhr.responseText);
 			$("#alert").addClass("alert alert-danger").text("Não foi possível salvar esta promoção.");
+		},
+		complete: function() {
+			$("#loader-form").fadeOut(800, function() {
+				$("#form-add-promo").fadeIn(250);
+				$("#loader-form").removeClass("loader");
+			});
 		}
 	});
 });
