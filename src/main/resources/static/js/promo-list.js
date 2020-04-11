08a2a97c4ad67e174ae079eae4388ed3b345d908
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-11 01:24:19 -0300

    Infinite Scroll em Ação

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,12 +1,17 @@
 var pageNumber = 0;
 
+$(document).ready(function() {
+	$("#loader-img").hide();
+	$("#fim-btn").hide();
+});
+
 //efeito infinite scroll
 $(window).scroll(function() {
 	
 	var scrollTop = $(this).scrollTop();
 	var conteudo = $(document).height() - $(window).height();
 	
-	console.log('scrollTop: ', scrollTop, ' | ', 'conteudo: ', conteudo);
+	//console.log('scrollTop: ', scrollTop, ' | ', 'conteudo: ', conteudo);
 	
 	if(scrollTop >= conteudo) {
 		pageNumber++;
@@ -25,8 +30,28 @@ function loadByScrollBar(pageNumber) {
 		data: {
 			page: pageNumber
 		},
+		beforeSend: function() {
+			$("#loader-img").show();
+		},
 		success: function(response) {
-			console.log("resposta > ", response);
+			//console.log("resposta > ", response);
+			
+			console.log("lista > ", response.length);
+			
+			if(response.length > 150) {
+				$(".row").fadeIn(250, function() {
+					$(this).append(response);
+				});
+			} else {
+				$("#fim-btn").show();
+				$("#loader-img").removeClass("loader");
+			}
+		},
+		error: function(xhr) {
+			alert("Ops, ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
+		},
+		complete: function() {
+			$("#loader-img").hide();
 		}
 	})
 }
