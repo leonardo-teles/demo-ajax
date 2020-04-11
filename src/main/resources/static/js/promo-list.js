280c510c6b7e82cfe53a07def388012e8311a153
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-11 00:26:57 -0300

    Criando o Infinite Scroll

---
+++ src/main/resources/static/js/promo-list.js
@@ -0,0 +1,13 @@
+//efeito infinite scroll
+$(window).scroll(function() {
+	
+	var scrollTop = $(this).scrollTop();
+	var conteudo = $(document).height() - $(window).height();
+	
+	console.log('scrollTop: ', scrollTop, ' | ', 'conteudo: ', conteudo);
+	
+	if(scrollTop >= conteudo) {
+		console.log("******************");
+	}
+	
+});
