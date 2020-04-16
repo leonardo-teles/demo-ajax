2fe6f0b72a5b11a253286d74993e77600511233f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-04-16 17:31:57 -0300

    Integrando a DWR ao Spring

--- src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
@@ -1,12 +1,28 @@
 package com.mballem.demoajax;
 
+import org.directwebremoting.spring.DwrSpringServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource(locations = "classpath:dwr-spring.xml")
 @SpringBootApplication
 public class DemoAjaxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean<DwrSpringServlet> dwrSpringServlet() {
+		DwrSpringServlet dwrServlet = new DwrSpringServlet();
+		
+		ServletRegistrationBean<DwrSpringServlet> registrationBean = new ServletRegistrationBean<>(dwrServlet, "/dwr/*");
+		registrationBean.addInitParameter("debug", "true");
+		registrationBean.addInitParameter("activeReverseAjaxEnabled", "true");
+		
+		return registrationBean;
 	}
 }
